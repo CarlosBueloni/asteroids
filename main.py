581c9e340f4de107b82d8d4d20ddf664e663ed03
CarlosBueloni <carlosbueloni@gmail.com>
2026-04-24 11:19:59 -0300

    add asteroid and asteroidfield modules

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver} ")
@@ -10,7 +12,14 @@ def main():
     pygame.init()
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
     Player.containers = (updatable, drawable)
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable)
+
+    asteroidfiled = AsteroidField()
+
     player = Player(SCREEN_WIDTH / 2,
                     SCREEN_HEIGHT / 2)
     clock = pygame.time.Clock()
